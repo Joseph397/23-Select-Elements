a32fc1a83cd190d8899274f226ca49645de41657
Joseph397 <gramkracker88@gmail.com>
2020-08-30 15:57:22 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// Select the element or group of elements that we want
+// Decide the effect we want to apply to the selection
+
+//getElementByid('element')
+
+// document.getElementById('btn').style.backgroundColor = 'blue';
+// document.getElementById('btn').style.color = 'white';
+
+// Can assign doucment.getElementById() to a variable 
+// below does the same as the code above more simply
+
+const btn = document.getElementById('title');
+btn.style.backgroundColor = 'blue'
+btn.style.color = 'white'
